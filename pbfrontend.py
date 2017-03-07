e3023b0d80ecbf7080a0a34e0a26e40eef2e757f
MathPotate <ddrisc2@live.spcollege.edu>
2017-03-07 13:02:49 -0500

    Renamed backend references

--- pbfrontend.py
+++ pbfrontend.py
@@ -16,24 +16,24 @@ def get_selected_row(event): #event handler for selecting an entry
     e4.insert(END,selected_tuple[4])
 def view_command():  
     list1.delete(0,END)
-    for row in backend.view():
+    for row in pbbackend.view():
         list1.insert(END,row)
 def search_command():
     list1.delete(0,END)
-    for row in backend.search(firstName_text.get(),lastName_text.get(),location_text.get(),number_text.get()):
+    for row in pbbackend.search(firstName_text.get(),lastName_text.get(),location_text.get(),number_text.get()):
         list1.insert(END,row)
 
 def add_command():
-    backend.insert(firstName_text.get(),lastName_text.get(),location_text.get(),number_text.get())
+    pbbackend.insert(firstName_text.get(),lastName_text.get(),location_text.get(),number_text.get())
     list1.delete(0,END)
     list1.insert(END,(firstName_text.get(),lastName_text.get(),location_text.get(),number_text.get()))
 
 def delete_command(): 
-    backend.delete(selected_tuple[0])
+    pbbackend.delete(selected_tuple[0])
     view_command()
 
 def update_command():
-    backend.update(selected_tuple[0],firstName_text.get(),lastName_text.get(),location_text.get(),number_text.get())
+    pbbackend.update(selected_tuple[0],firstName_text.get(),lastName_text.get(),location_text.get(),number_text.get())
     view_command()
 
 
